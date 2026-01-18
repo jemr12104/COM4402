7bae4fc9f46baf5e6a824d5db94d08f5ae7b6784
jemr12104 <jemr12104@gmail.com>
2026-01-18 19:25:38 +0000

    Added question loop, score update, and feedback messages for each question

--- 2410521_COM4402_Quiz/quiz.py
+++ 2410521_COM4402_Quiz/quiz.py
@@ -2,6 +2,9 @@
 # Student: Jefferson Eduardo Mendoza Rios
 # Student Number: 2410521
 
+
+# This function shows the question and the options.
+
 def ask_question(question, options):
     print(question)
 
@@ -18,29 +21,49 @@ def ask_question(question, options):
 
     return user_answer
 
+# This function checks if the answer is correct or not.
 
 def check_answer(user_answer, correct_answer):
     if user_answer == correct_answer:
         print("Correct")
         return True
+       
     else:
         print("Incorrect")
         return False
 
-
-
+# This is the main quiz function.
 
 def run_quiz():
-    question = "What is 2 * 8?"
-    options = ["1) 3", "2) 4", "3) 5", "4) 16"]
-    correct_answer = "4"
+    questions = [
+        "What is 2 * 8?",
+        "What is 10 / 2?",
+        "What is 3 + 4?"
+    ]
 
-    answer = ask_question(question, options)
+    options = [
+        ["1) 3", "2) 4", "3) 5", "4) 16"],
+        ["1) 2", "2) 5", "3) 10", "4) 12"],
+        ["1) 5", "2) 6", "3) 7", "4) 8"]
+    ]
 
-    result = check_answer(answer, correct_answer)
+    correct_answers = ["4", "2", "3"]
 
-    print("Result:", result)
 
+    score = 0
+    
+    for i in range(len(questions)):
+        answer = ask_question(questions[i], options[i])
+        result = check_answer(answer, correct_answers[i])
+
+        print("Your answer was:", answer)
+        print("Next question.")
+        print("----------------------")
+
+        if  result == True:
+            score = score + 1
+
+    print("You answered", score, "out of", len(questions), "correct.")
     print("Quiz finished.")
 
 
